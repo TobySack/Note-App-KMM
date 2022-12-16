a0fb16c75379d7465f6f85d2a40f39ea3cbe9787
Toby <tobyasack@hotmail.com>
2022-12-16 14:46:44 -0700

    Finish adding dependency injection

--- androidApp/src/main/java/com/example/noteappkmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/noteappkmm/android/MainActivity.kt
@@ -8,31 +8,15 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.noteappkmm.android.noteList.NoteListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                }
+                NoteListScreen()
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
